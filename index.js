32de50a2d7e0c5b0ab93ef29d1b08bcd65193932
cryptolemming <aliayoub22@gmail.com>
2016-03-19 20:29:08 -0400

    added connect binding and action creators

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { createStore, combineReducers } from 'redux';
+import { Provider, connect } from 'react-redux';
 
 const low = ((state=0, action) => {
 	if(action.type === 'UP') {
@@ -18,68 +19,61 @@ const high = ((state=9, action) => {
 
 class Basement extends React.Component {
 	render() {
-		var store = this.context.store;
-		var state = store.getState().high;
+		var state = this.props.high;
 		return (
 			<div>basement! {state}
-				<button onClick={() => { store.dispatch({type: 'DOWN'}) }}>DOWN</button>
+				<button onClick={this.props.down}>DOWN</button>
 			</div>
 		);
 	}
-}
-
-Basement.contextTypes = {
-	store: React.PropTypes.object
 }
 
 class Middle extends React.Component {
 	render() {
 		return (
-			<div>middle! <Basement /></div>
+			<div>middle! <XBasement /></div>
 		);
 	}
 }
 
 class TopLevel extends React.Component {
-	componentDidMount() {
-		const store = this.context.store;
-		store.subscribe(() => {
-			this.forceUpdate();
-		});
-	}
 	render() {
-		const store = this.context.store;
-		const state = store.getState().low;
+		var state = this.props.low;
 		return (
 			<div>top! {state}
-				<button onClick={() => { store.dispatch({type: 'UP'}) }}>UP</button>
-				<Middle store={store} />
+				<button onClick={this.props.up}>UP</button>
+				<Middle />
 			</div>
 		);
 	}
-};
-
-TopLevel.contextTypes = {
-	store: React.PropTypes.object
 }
 
-class Provider extends React.Component {
-	// pass the state to itself as a prop
-	getChildContext() {
-		return {
-			store: this.props.store
-		}
+const mapState = (state) => {
+	return {
+		low: state.low
 	}
-	render() {
-		// the HTML from the TopLevel component
-		return this.props.children;
-	}
-};
-
-// set childContext types for the Provider
-Provider.childContextTypes = {
-	store: React.PropTypes.object
 }
+
+const mapDispatch = (dispatch) => {
+	return {
+		up: () => dispatch({type: 'UP'})
+	}
+}
+
+const mapStateBasement = (state) => {
+	return {
+		high: state.high
+	}
+}
+
+const mapDispatchBasement = (dispatch) => {
+	return {
+		down: () => dispatch({type: 'DOWN'})
+	}
+}
+
+const XTopLevel = connect(mapState, mapDispatch)(TopLevel);
+const XBasement = connect(mapStateBasement, mapDispatchBasement)(Basement);
 
 render((
 	<Provider store={createStore(combineReducers({low: low, high: high}))}>
