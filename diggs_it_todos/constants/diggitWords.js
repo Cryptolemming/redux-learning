344a2ccf118d47fd842509eae7f68ecf2876c5a2
cryptolemming <aliayoub22@gmail.com>
2016-04-12 20:43:07 -0400

    section 1 start

--- diggs_it_todos/constants/diggitWords.js
+++ diggs_it_todos/constants/diggitWords.js
@@ -3,7 +3,7 @@ const DIGGIT_WORDS =
 		'love', 'music', 'code', 'program', 'learn', 'cool', 'build',
 		'athingaweek', 'slack', 'programming', 'coding', 'song', 'javascript',
 		'steak', 'avocadoes', 'avocado', 'sweet potatoes', 'sweet potato',
-		'cat', 'cats', 'pets', 'pet'
+		'cat', 'cats', 'pets', 'pet', 'friend', 'friends', 'fun'
 	];
 
 module.exports = DIGGIT_WORDS;
