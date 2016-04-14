580f685030782541a669c0141c0035312507b9ea
cryptolemming <aliayoub22@gmail.com>
2016-04-14 09:53:11 -0400

    searchbar to class

--- react-redux-course/section1/src/components/search_bar.js
+++ react-redux-course/section1/src/components/search_bar.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const SearchBar = () => {
-	return <input />
-};
+
+class SearchBar extends Component {
+	render() {
+		return <input onChange={this.onInputChange} />;
+	}
+
+	onInputChange(e) {
+		console.log(e.target.value);
+	}
+}
 
 export default SearchBar;
